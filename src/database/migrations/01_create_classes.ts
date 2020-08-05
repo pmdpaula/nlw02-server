4a269a2ab77036e46323b04fddbdeaff7edead69
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2020-08-05 01:48:04 -0300

    Correção das tabelas

--- src/database/migrations/01_create_classes.ts
+++ src/database/migrations/01_create_classes.ts
@@ -1,21 +1,21 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-  return knex.schema.createTable('class_schedule', (table) => {
+  return knex.schema.createTable('classes', (table) => {
     table.increments('id').primary();
 
     table.string('subject').notNullable();
     table.decimal('cost').notNullable();
 
-    table.integer('class_id')
+    table.integer('user_id')
       .notNullable()
       .references('id')
-      .inTable('classes')
+      .inTable('users')
       .onUpdate('CASCADE')
       .onDelete('CASCADE');
   });
 }
 
 export async function down(knex: Knex) {
-  return knex.schema.dropTable('class_schedule');
+  return knex.schema.dropTable('classes');
 }
--- src/database/migrations/02_create_class_schedule.ts
+++ src/database/migrations/02_create_class_schedule.ts
@@ -1,7 +1,7 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-  return knex.schema.createTable('classes', (table) => {
+  return knex.schema.createTable('class_schedule', (table) => {
     table.increments('id').primary();
 
     table.integer('week_day').notNullable();
@@ -18,5 +18,5 @@ export async function up(knex: Knex) {
 }
 
 export async function down(knex: Knex) {
-  return knex.schema.dropTable('classes');
+  return knex.schema.dropTable('class_schedule');
 }
