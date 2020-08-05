f5dcd18b6dfea1e5a00c5d43f3cb8193c4fcb6ad
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2020-08-05 00:31:33 -0300

    Exec yarn migrate:latest criando a base e tabelas

---
+++ src/database/migrations/01_create_classes.ts
@@ -0,0 +1,21 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('class_schedule', (table) => {
+    table.increments('id').primary();
+
+    table.string('subject').notNullable();
+    table.decimal('cost').notNullable();
+
+    table.integer('class_id')
+      .notNullable()
+      .references('id')
+      .inTable('classes')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('class_schedule');
+}
